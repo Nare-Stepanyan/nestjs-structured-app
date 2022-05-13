4a8188e0b58cb07cb78f2e0e4545ecdc1f6a1276
Hovo Sardaryan <hovosardaryan@Hovos-MacBook-Pro.local>
2022-05-14 03:36:04 +0400

    Issue with connecting db when runneng app inside docker

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,3 +1,4 @@
+import { Product } from '@app/db/entity/product.entity';
 import { ProductRepository } from '@app/db/repository/product.repository';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -5,7 +6,7 @@ import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductRepository])],
+  imports: [TypeOrmModule.forFeature([ProductRepository, Product])],
   controllers: [ProductController],
   providers: [ProductService],
 })
